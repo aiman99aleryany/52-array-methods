7488f904a6f1013a68e2084e3425054829233af9
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-20 00:14:13 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,20 @@
+// Given the following array:
+
+// const students = [
+// { id: 1, name: "Luca", surname: "Rossi", age: 20 },
+// { id: 2, name: "Mario", surname: "Bianchi", age: 24 },
+// { id: 3, name: "Giovanni", surname: "Brambilla", age: 30 },
+// ];
+// Remove the last element of the array and add the following one at the end of the array: { id: 3, name: "Francesco", surname: "Verdi", age: 41 }
+
+const students = [
+  { id: 1, name: "Luca", surname: "Rossi", age: 20 },
+  { id: 2, name: "Mario", surname: "Bianchi", age: 24 },
+  { id: 3, name: "Giovanni", surname: "Brambilla", age: 30 },
+];
+
+students.pop();
+
+students.push({ id: 3, name: "Francesco", surname: "Verdi", age: 41 });
+
+console.log(students);
